18d51461019f22790c50a7397edb7def64b9b21b
hkalife <henriquekalife@gmail.com>
2022-03-21 08:55:33 -0300

    feat: implement filter by average rating

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,11 +1,27 @@
 exports.Query = {
-  products: (parent, { filter }, { products }) => {
+  products: (parent, { filter }, { products, reviews }) => {
     let filteredProducts = products;
 
     if (filter) {
-      if (filter.onSale === true) {
+      const { onSale, avgRating } = filter;
+      if (onSale) {
         filteredProducts = filteredProducts.filter(product => {
-          return product.onSale
+          return product.onSale;
+        })
+      }
+      if ([1,2,3,4,5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter(product => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+          reviews.forEach(review => {
+            if(review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+          const avgProductRating = sumRating / numberOfReviews;
+
+          return avgProductRating >= avgRating;
         })
       }
     }
