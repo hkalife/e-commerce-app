bfddfe4c811ecaeb61863616730d6bff5d16c5bc
hkalife <henriquekalife@gmail.com>
2022-03-21 08:18:08 -0300

    fix: fix double query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -15,7 +15,7 @@ exports.Query = {
   product: (parent, { id }, { products }) => {
     return products.find(product => product.id === id);
   },
-  products: (parent, args, { products }) => products,
+  categories: (parent, args, { categories }) => categories,
   category: (parent, { id }, { categories }) => {
     return categories.find((category) => category.id === id);
   }
