3cadb3d17b301e221ea83ce8ffb44cd497b2de4b
hkalife <henriquekalife@gmail.com>
2022-03-22 08:20:35 -0300

    feat: add product

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -8,9 +8,33 @@ exports.Mutation = {
       id: uuid(),
       name
     }
-    
+
     categories.push(newCategory);
 
     return newCategory;
+  },
+  addProduct: (parent, { input }, { products }) => {
+    const { 
+      name,
+      image,
+      price,
+      onSale,
+      quantity,
+      categoryId
+    } = input;
+
+    const newProduct = {
+      id: uuid(),
+      name,
+      image,
+      price,
+      onSale,
+      quantity,
+      categoryId
+    };
+
+    products.push(newProduct);
+
+    return newProduct;
   }
 }
